0bd61ffd4c9e996174a514ac0d6746fd83a19fec
genoxci <just_inntime@outlook.com>
2025-08-05 05:56:24 +0800

    Settings: Relocate 'Device name', match 'More info' with the rest of the buttons

---
+++ src/com/android/settings/deviceinfo/firmwareversion/MoreInfoPreferenceController.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.view.View;
+import android.widget.TextView;
+import android.content.Context;
+
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.widget.LayoutPreference;
+
+public class MoreInfoPreferenceController extends BasePreferenceController {
+
+    public MoreInfoPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return "";
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        
+        LayoutPreference preference = (LayoutPreference) screen.findPreference(getPreferenceKey());
+        if (preference != null) {
+            // Get the root view of your custom layout
+            View widgetFrame = preference.findViewById(android.R.id.widget_frame);
+            if (widgetFrame != null && widgetFrame.getParent() instanceof View) {
+                ((View) widgetFrame.getParent()).setBackground(null);
+            }
+            
+            // Populate the TextViews manually
+            TextView titleView = preference.findViewById(android.R.id.title);
+            TextView summaryView = preference.findViewById(android.R.id.summary);
+            
+            if (titleView != null) {
+                titleView.setText(mContext.getString(R.string.more_device_info));
+            }
+            if (summaryView != null) {
+                summaryView.setVisibility(View.GONE); // Hide summary for "More info"
+            }
+        }
+    }
+}
